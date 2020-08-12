7ab7d102f35207042eaeb12920bfb0259c0c9e73
Johnny Rice <jrice@labact.com>
2020-08-12 18:29:30 -0400

    Provider starter create-react-app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+// @see https://reactjs.org/docs/rendering-elements.html#updating-the-rendered-element
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+// @see https://reactjs.org/docs/create-a-new-react-app.html
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Ok.
         </p>
         <a
           className="App-link"
@@ -19,6 +51,8 @@ function App() {
           Learn React
         </a>
       </header>
+      // See https://reactjs.org/docs/rendering-elements.html#updating-the-rendered-element
+      <Clock date={new Date()} />,
     </div>
   );
 }
