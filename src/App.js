2c4608479428eb56a18576e50c0fe0b93fd53f36
Johnny Rice <jrice@labact.com>
2020-08-12 22:01:12 -0400

    Revert and merge

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import Clock from './Clock.js'
 
-const App = () => {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="App-logo" src={logo} alt="logo"/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-
-      <Clock/>
     </div>
   )
 }
